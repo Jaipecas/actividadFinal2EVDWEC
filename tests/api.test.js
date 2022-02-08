ebe8cca3e926d4535b1e8bccf9d6acb1b50baa72
Alvaro Maceda <alvaro@alvaromaceda.es>
2022-02-08 09:11:39 +0100

    adds tests placeholder

---
+++ tests/api.test.js
@@ -0,0 +1,16 @@
+describe('(your function for doing API requests here)', () => {
+
+  it('fetchs the URL')
+
+  it('returns the json data')
+
+  it('includes the authorization token')
+
+  describe('when the api returns an error', () => {
+    it('returns the error');
+  });
+
+  describe('when the API returns 401', () => {
+    it('redirects to login');
+  });
+});
