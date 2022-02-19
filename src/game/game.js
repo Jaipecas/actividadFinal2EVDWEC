d4716cf48de2dda94bb4526785a5ed3fc0a259ab
Jaime Perez Castellanos <jaipecas89@gmail.com>
2022-02-19 13:38:55 +0100

    Se refactoriza la parte 3

--- src/game/game.js
+++ src/game/game.js
@@ -1,42 +1,95 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../game/game.css';
 import '../common/common.css'
+import '../game/game.css';
 import 'regenerator-runtime/runtime';
 import 'node-fetch';
 import {
     Movement
 } from '../classes/movement';
 
-const urlParams = new URLSearchParams(location.search);
-const gameIdSpan = document.getElementById('game-id');
-let arrayMovements = [];
 
-function insertMarkInBoard(movement, mark) {
-    let cell = document.querySelector(`[data-row = '${movement[0]}'][data-column = '${movement[1]}']`);
-    cell.innerText = mark;
+const loader = document.getElementById('loader');
+const beforeButton = document.getElementById('back');
+const nextButton = document.getElementById('next');
+const returnButton = document.getElementById('return');
+const gameIdSpan = document.getElementById('game-id');
+
+let loadedMovements = [];
+let positionMovement = 0;
+
+const getMovementCell = (movement) => document.querySelector(`[data-row = '${movement[0]}'][data-column = '${movement[1]}']`);
+
+function insertMovementBoard(movement) {
+    let cell = getMovementCell(movement);
+    positionMovement % 2 === 0 ? cell.innerText = 'X' : cell.innerText = 'O';
 }
 
-async function loadArrayMovements() {
+function removeMovementBoard(movement) {
+    let cell = getMovementCell(movement);
+    cell.innerText = "";
+}
+
+function activeBeforeButton() {
+    positionMovement < 1 ? beforeButton.disabled = true : beforeButton.disabled = false;
+}
+
+function activeNextButton(numberLoadedMovements) {
+    numberLoadedMovements - 1 > positionMovement ? nextButton.disabled = false : nextButton.disabled = true;
+}
+
+function nextMovement() {
+    positionMovement += 1;
+    insertMovementBoard(loadedMovements[positionMovement])
+    activeNextButton(loadedMovements.length);
+    activeBeforeButton();
+}
+
+function beforeMovement() {
+    removeMovementBoard(loadedMovements[positionMovement]);
+    positionMovement -= 1;
+    activeNextButton(loadedMovements.length);
+    activeBeforeButton();
+}
+
+async function loadFirstMovement(game, firstMovement) {
+    loader.classList.add('active');
+    const movement = await Movement.getMovement(localStorage.token, game, firstMovement);
+    loadedMovements.push(movement.movement);
+    insertMovementBoard(loadedMovements[positionMovement]);
+    loader.classList.remove('active');
+    return movement.next;
+}
+
+
+async function loadMovements(game, firstMovement) {
     try {
         let movementActually;
-        let nextMovement = urlParams.get('movement');
-        do {
-            movementActually = await Movement.getMovement(localStorage.token, urlParams.get('game'), nextMovement);
-            arrayMovements.push(movementActually.movement);
-            nextMovement = movementActually.next;
-        } while (nextMovement);
+        let nextMovement = await loadFirstMovement(game, firstMovement);
 
+        while (nextMovement) {
+            movementActually = await Movement.getMovement(localStorage.token, game, nextMovement);
+            loadedMovements.push(movementActually.movement);
+            nextMovement = movementActually.next;
+            activeNextButton(loadedMovements.length);
+        }
     } catch (error) {
         console.log(error);
         if (error === 401) location = location.origin;
     }
 }
 
-function loadWindow() {
-    loadArrayMovements();
-    gameIdSpan.innerText = urlParams.get('game');
+function addHandlers() {
+    nextButton.addEventListener('click', nextMovement);
+    beforeButton.addEventListener('click', beforeMovement);
+    returnButton.addEventListener('click', () => history.back());
+}
 
+function loadWindow() {
+    const urlParams = new URLSearchParams(location.search);
+    addHandlers();
+    gameIdSpan.innerText = urlParams.get('game');
+    loadMovements(urlParams.get('game'), urlParams.get('movement'));
 }
 
 
